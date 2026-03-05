dfbd73b8d914e1eb39559b04a7e7a5a97b8ae08f
Mohyminul Islam <mohyminulmunna23@gmail.com>
2026-03-05 20:06:09 +0600

    make products, analytics and users apis

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Create app
 const app = express();
@@ -11,7 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@mohyminulislam.uwhwdlk.mongodb.net/?appName=Mohyminulislam`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,14 +27,43 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    await client.connect();
     const database = client.db("donezo");
+    const usersCollections = database.collection("users");
+    const analyticsCollections = database.collection("analytics");
+    const productsCollections = database.collection("products");
+
+    app.get("/users", async (req, res) => {
+      try {
+        const users = await usersCollections.find().toArray();
+        res.status(200).json(users);
+      } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+    app.get("/analytics", async (req, res) => {
+      try {
+        const analytics = await analyticsCollections.find().toArray();
+        res.status(200).json(analytics);
+      } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+    app.get("/products", async (req, res) => {
+      try {
+        const products = await productsCollections.find().toArray();
+        res.status(200).json(products);
+      } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("✅ You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
