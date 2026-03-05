1fe497ef8e7f01ceba3c949044b05d60aaf33518
Mohyminul Islam <mohyminulmunna23@gmail.com>
2026-03-05 17:57:58 +0600

    connected mongoDB successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import dependencies
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 // Create app
 const app = express();
@@ -9,6 +10,34 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@mohyminulislam.uwhwdlk.mongodb.net/?appName=Mohyminulislam`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("donezo");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Default route
 app.get("/", (req, res) => {
